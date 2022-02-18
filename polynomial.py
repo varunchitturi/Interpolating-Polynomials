653cb89bbf3a565e509bc27dcf757aeafad5064f
Varun Chitturi <varunchitturi@icloud.com>
2022-02-18 14:52:08 -0800

    Add docstrings

--- polynomial.py
+++ polynomial.py
@@ -1,6 +1,14 @@
-class Polynomial:
-    
+from typing import List, Any
+from coordinate import Coordinate
 
-    coeffecients = []
+class Polynomial:
+
+    coefficients: List[int] = []
 
     def __init__(self, *points):
+        """
+
+        :param points: List of points that the polynomial goes through
+        :type points: Coordinate
+        """
+        print()
