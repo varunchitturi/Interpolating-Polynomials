aceb88f8e970fce544a54556267abc2646aa7007
Kalkulator413 <kalkin413@gmail.com>
2022-02-18 16:16:30 -0800

    cleaned up most of Polynomial.__str__() and added an integral method

--- polynomial.py
+++ polynomial.py
@@ -1,32 +1,34 @@
 import numpy as np
+import re
 
-# need to make it dispay ints without the .0 (can probably
-# do this by checking sol[0] % 1 or smth) and round floats
-# to 2-3 digits. also + followed by minus should become -1
-# (this part can be done with a clean string method?)
-# also, any 1.0's should be removed (can be done with same
-# clean string method)
+# maybe clean up x^1 into just x and don't display 0 terms
 class Polynomial:
-    
-    coeffecients = []
-
-    def __init__(self, points):
+    def __init__(self, points, coeffs=None):
         assert(type(points == list))
         # points list should look like [[x1, y1], [x2, y2], ... ]
         # or [Point1, Point2, Point3, ...]
-        self.points = points
-        self.degree = len(points) - 1
-        constants = np.array([p[1] for p in points])
-        #constants = [point.x for point in list]
+        if points:
+            self.points = points
+            self.degree = len(points) - 1
+            constants = np.array([p[1] for p in points])
+            #constants = [point.x for point in list]
+            x_vals = self.get_x_coeffs()
+            self.coeffs = np.linalg.solve(x_vals, constants)
+        elif coeffs:
+            self.coeffs = coeffs
+            self.degree = len(coeffs) - 1
+        else:
+            assert("Please enter either a list of points or a list of coefficients")
 
-        coeffs = self.get_coeffs()
-
-        self.sol = np.linalg.solve(coeffs, constants)
 
     def __str__(self):
-        return str_helper(self.sol, self.degree)
+        return clean_str(str_helper(self.coeffs, self.degree))
 
-    def get_coeffs(self):
+    def get_integral(self):
+        coeffs = integral_helper(self.degree, self.coeffs)
+        return Polynomial(None, coeffs=coeffs)
+
+    def get_x_coeffs(self):
         #take in a list of x values, return a matrix of coefficients
         coeffs = []
 
@@ -38,14 +40,38 @@ class Polynomial:
             coeffs.append(eq)
         return np.array(coeffs)
 
-def str_helper(sols, degree):
+    
+def integral_helper(degree, coeffs):
     if degree == 0:
-        return str(sols[0])
-    return f'{sols[0]}x^{degree} + ' + str_helper(sols[1:], degree - 1)
+        return []
+    return [coeffs[0] * degree] + integral_helper(degree - 1, coeffs[1:])
 
-quadratic = Polynomial([[1, 2], [2, 4], [3, 8]])
+def str_helper(sols, degree):
+    a = sols[0]
+    a = round(a * 1000)/1000.0
+    if a % 1.0 in [0, 0.0]:
+        a = int(a)
+
+    if degree == 0:
+        if a == 1:
+            return ''
+        elif a == -1:
+            return '-'
+        return str(a) if a != 1 else ''
+    else:
+        if a == 1:
+            return f'x^{degree} + ' + str_helper(sols[1:], degree - 1)
+        elif a == -1:
+            return f'-x^{degree} + ' + str_helper(sols[1:], degree - 1)
+        return f'{a}x^{degree} + ' + str_helper(sols[1:], degree - 1)
+
+def clean_str(s):
+    return s.replace("+ -", '- ')
+
+poly = Polynomial([[1, 2], [2, 4], [3, 8], [0, 5]])
 # quadratic = Polynomial(Point(1, 2), Point(2, 4), Point(3, 8))
-print(quadratic)
+print(poly)
+print(poly.get_integral())
 
 
 
