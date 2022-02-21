64255dcb874f4feec7249be0ea3268651c56b7ea
Kalkulator413 <kalkin413@gmail.com>
2022-02-21 11:58:03 -0800

    added  qs 1 and 2

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from Polynomial import Polynomial, Point
 
 
 def main():
-    poly = Polynomial(Point(1, 2), Point(2, 4), Point(3, 8), Point(0, -5))
-    print('calculated polynomial:', poly)
-    print('derivative:', poly.get_derivative())
-    print('integral', poly.get_integral())
 
+    print("Question 1:")
+    poly = Polynomial(Point(1,29), Point(-1,-35), Point(2,31), Point(-3,-19))
+    print('calculated polynomial:', poly, '\n')
+
+    print('Question 2:')
+    poly = Polynomial(Point(-1, 0.5), Point(0,1), Point(1,2), Point(2,4), Point(3,8))
+    print('calculated polynomial:', poly, '\n')
 
 if __name__ == "__main__":
     main()
